c42183bef5b7727862e961fbac8a6d7d81597338
Fabian Nowak <hey-furb@web.de>
2025-12-05 15:33:23 +0100

    refactor coordinator.py

--- custom_components/mazda_6e/coordinator.py
+++ custom_components/mazda_6e/coordinator.py
@@ -7,29 +7,24 @@ from .const import DOMAIN, UPDATE_INTERVAL
 from .models import Mazda6eVehicle
 
 
-
 class Mazda6eCoordinator(DataUpdateCoordinator):
-    def __init__(self, hass, entry):
-        self.api = Mazda6EApi(
-            aiohttp_client.async_get_clientsession(hass),
-            entry.data["email_enc"],
-            entry.data["password_enc"],
-        )
-        self.deviceid = entry.data["deviceid"]
-
+    def __init__(self, hass, config_entry, mazda6eApi):
+        """Initialize my coordinator."""
         super().__init__(
             hass,
             logger=__import__("logging").getLogger(DOMAIN),
             name=DOMAIN,
+            config_entry=config_entry,
             update_interval=timedelta(seconds=UPDATE_INTERVAL),
         )
+        self.api = mazda6eApi
 
     async def _async_update_data(self):
-        vehicles: list[Mazda6eVehicle] = await self.api.async_get_vehicles(self.deviceid)
+        vehicles: list[Mazda6eVehicle] = await self.api.async_get_vehicles(self.api.deviceid)
         vehicle_status = {}
 
         for veh in vehicles:
-            status = await self.api.async_get_vehicle_status(veh.vehicle_id, self.deviceid)
+            status = await self.api.async_get_vehicle_status(veh.vehicle_id, self.api.deviceid)
             vehicle_status[veh.vehicle_id] = {
                 "vehicle": veh,
                 "status": status,
