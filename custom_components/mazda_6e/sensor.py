36a8a99510abc0bc98f347db3b641a9378333806
Fabian Nowak <hey-furb@web.de>
2025-12-07 09:23:31 +0100

    fixed sensor.py

--- custom_components/mazda_6e/sensor.py
+++ custom_components/mazda_6e/sensor.py
@@ -1,5 +1,6 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.const import UnitOfEnergy, UnitOfLength
 
 from .const import DOMAIN
 
@@ -7,23 +8,56 @@ from .const import DOMAIN
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    async_add_entities([
-        Mazda6eSocSensor(coordinator),
-        Mazda6eRangeSensor(coordinator),
-    ])
+    entities = []
+
+    # Der Coordinator liefert ein dict: { vehicle_id: { "vehicle": Mazda6eVehicle, "status": {...} } }
+    for vehicle_id, data in coordinator.data.items():
+        vehicle = data["vehicle"]
+
+        entities.append(Mazda6eSocSensor(coordinator, vehicle_id, vehicle))
+        entities.append(Mazda6eRangeSensor(coordinator, vehicle_id, vehicle))
+
+    async_add_entities(entities)
 
 
-class Mazda6eSocSensor(CoordinatorEntity, SensorEntity):
+class Mazda6eBaseEntity(CoordinatorEntity, SensorEntity):
+    """Gemeinsame Basis für alle Sensoren"""
+
+    def __init__(self, coordinator, vehicle_id, vehicle):
+        super().__init__(coordinator)
+        self.vehicle_id = vehicle_id
+        self.vehicle = vehicle
+
+        # Eindeutige ID pro Entity & Fahrzeug
+        self._attr_unique_id = f"{vehicle_id}_{self.__class__.__name__}"
+
+        # Benutzerfreundlicher Name
+        self._attr_name = f"{vehicle.model_name} {self.__class__.__name__}"
+
+
+class Mazda6eSocSensor(Mazda6eBaseEntity):
+    """SOC-Sensor"""
+
     _attr_icon = "mdi:battery"
+    _attr_native_unit_of_measurement = "%"
 
     @property
     def native_value(self):
-        return self.coordinator.data[self.vehicle_id]["vehicleStatus"]["soc"]
+        data = self.coordinator.data.get(self.vehicle_id)
+        if not data:
+            return None
+        return data["status"].get("soc")
 
 
-class Mazda6eRangeSensor(CoordinatorEntity, SensorEntity):
+class Mazda6eRangeSensor(Mazda6eBaseEntity):
+    """Reichweiten-Sensor"""
+
     _attr_icon = "mdi:map-marker-distance"
+    _attr_native_unit_of_measurement = UnitOfLength.KILOMETERS
 
     @property
     def native_value(self):
-        return self.coordinator.data[self.vehicle_id]["vehicleStatus"]["drvMileage"]
+        data = self.coordinator.data.get(self.vehicle_id)
+        if not data:
+            return None
+        return data["status"].get("drvMileage")
